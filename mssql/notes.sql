267539f08c5f16439d6d8a7dc0264a32fa79ef8d
Talgat <tairovtalgat@gmail.com>
2019-03-03 11:03:38 +0600

    add notes for ms sql server

--- mssql/notes.sql
+++ mssql/notes.sql
@@ -21,3 +21,15 @@ CAST()
 getutcdate() 
 current_timestamp() 
 dateadd()
+
+
+
+
+-- ПОСМОТРЕТЬ РАЗМЕРЫ БАЗ
+SELECT      sys.databases.name,
+            CONVERT(VARCHAR,SUM(size)*8/1024)+' MB' AS [Total disk space]
+FROM        sys.databases   
+JOIN        sys.master_files  
+ON          sys.databases.database_id=sys.master_files.database_id
+GROUP BY    sys.databases.name
+ORDER BY    sys.databases.name
